4f0d225a3b5174c439013751cb7d37c3fbdee67d
Greg Rickaby <gregrickaby@gmail.com>
2022-01-10 07:14:00 -0600

    tailwind v3 upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      './components/**/*.{js,ts,jsx,tsx,md,mdx}',
-      './pages/**/*.{js,ts,jsx,tsx,md,mdx}'
-    ]
-  },
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}'
+  ],
   darkMode: 'media',
   theme: {
     extend: {
       container: {
         center: true
-      },
-      colors: {
-        gray: {
-          950: '#111'
-        }
       }
     }
   },
